c331670638c921e9775042c167f01e7bcb666db1
Astokchokxrselak <thedi3s323@gmail.com>
2021-09-19 12:11:12 -0400

    fuck fuck fuck (finally removed recipe)

--- Items/MortJavelin/MortJavelinItem.cs
+++ Items/MortJavelin/MortJavelinItem.cs
@@ -33,15 +33,6 @@ namespace MythosOfMoonlight.Items
 			item.UseSound = SoundID.Item1;
 		}
 
-		public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
-		}
-
 		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
 			var direction = new Vector2(speedX, speedY);
