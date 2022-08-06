095c6fcd76f6a09fd6450d292a47d674913b3871
Blink_Dagger <82193069+blinkdagger214748@users.noreply.github.com>
2022-08-06 23:00:46 +0800

    Comet Ember Added

---
+++ NPCs/Enemies/CometEmber.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MythosOfMoonlight.BiomeManager;
+using MythosOfMoonlight.Items.Materials;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using MythosOfMoonlight.Dusts;
+
+namespace MythosOfMoonlight.NPCs.Enemies
+{
+    public class CometEmber : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Comet Ember");
+            NPCID.Sets.NPCBestiaryDrawModifiers value = new(0) { Velocity = 1f };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, value);
+            NPCID.Sets.TrailCacheLength[Type] = 12;
+            NPCID.Sets.TrailingMode[Type] = 3;
+        }
+        public override void SetDefaults()
+        {
+            NPC.width = 38;
+            NPC.height = 42;
+            NPC.damage = 42;
+            NPC.lifeMax = 124;
+            NPC.defense = 2;
+            NPC.HitSound = SoundID.NPCHit39;
+            NPC.DeathSound = SoundID.NPCDeath57;
+            NPC.aiStyle = -1;
+            NPC.netAlways = true;
+            NPC.noTileCollide = true;
+            NPC.noGravity = true;
+            NPC.knockBackResist = 0f;
+            SpawnModBiomes = new int[]
+            {
+                ModContent.GetInstance<PurpleCometBiome>().Type
+            };
+        }
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+            {
+                BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Times.NightTime,
+                new BestiaryPortraitBackgroundProviderPreferenceInfoElement(ModContent.GetInstance<PurpleCometBiome>().ModBiomeBestiaryInfoElement),
+                new FlavorTextBestiaryInfoElement("//PlaceHolder// The Comet Ember, which spins in place, slowly moving up and down. Once the player is close, it tries to float above them and crash back down, exploding.")
+            });
+        }
+        private enum NState
+        {
+            Wander,
+            Hover,
+            Stomp
+        }
+        private NState State
+        {
+            get { return (NState)(int)NPC.ai[0]; }
+            set { NPC.ai[0] = (int)value; }
+        }
+        private void SwitchTo(NState state)
+        {
+            State = state;
+        }
+        public float PhaseTimer
+        {
+            get => NPC.ai[1];
+            set => NPC.ai[1] = value;
+        }
+        public float Timer
+        {
+            get => NPC.ai[2];
+            set => NPC.ai[2] = value;
+        }
+        public float Opaque;
+        public override void AI()
+        {
+            Timer++;
+            foreach (Player player in Main.player)
+            {
+                if (State == NState.Wander && Vector2.Distance(player.Center, NPC.Center) <= 100f && PhaseTimer > 60)
+                {
+                    PhaseTimer = 0;
+                    NPC.target = player.whoAmI;
+                    for (int i = 6; i <= 360; i += 6)
+                    {
+                        Vector2 pos = MathHelper.ToRadians(i).ToRotationVector2() * 70f;
+                        Dust dust = Dust.NewDustDirect(NPC.Center + pos, 1, 1, ModContent.DustType<PurpurineDust>());
+                        dust.velocity *= 0;
+                        dust.noGravity = true;
+                        dust.scale = 1.5f;
+                    }
+                    SoundEngine.PlaySound(SoundID.NPCDeath7, NPC.Center);
+                    SwitchTo(NState.Hover);
+                }
+                if (State == NState.Hover && Math.Abs(Main.player[NPC.target].Center.X - NPC.Center.X) <= 30f && PhaseTimer >= 120)
+                {
+                    PhaseTimer = 0;
+                    NPC.velocity = new Vector2(0, .1f);
+                    SwitchTo(NState.Stomp);
+                }
+            }
+            Player target = Main.player[NPC.target];
+            switch (State)
+            {
+                case NState.Wander:
+                    PhaseTimer++;
+                    NPC.velocity.Y = (float)Math.Sin(MathHelper.ToRadians(Timer * 1.33f));
+                    Opaque = 2f;
+                    break;
+                case NState.Hover:
+                    Opaque = 2f - (float)Math.Pow(.985f, (float)Math.Abs(target.Center.X - NPC.Center.X));
+                    PhaseTimer++;
+                    if (PhaseTimer > 30)
+                    {
+                        MoMNPC.EscapeCheck(1200f, 1200f, NPC);
+                        NPC.velocity = Vector2.Lerp(NPC.velocity, (target.Center - new Vector2(0, 300) - NPC.Center) / 50f, .1f);
+                    }
+                    else
+                    {
+                        NPC.velocity.Y = (float)Math.Sin(MathHelper.ToRadians(Timer * 1.33f));
+                    }
+                    break;
+                case NState.Stomp:
+                    Opaque = 1f;
+                    PhaseTimer++;
+                    if (NPC.Center.Y < target.Center.Y - 100) NPC.noTileCollide = true;
+                    else NPC.noTileCollide = false;
+                    NPC.velocity.Y += .5f;
+                    if ((NPC.collideY || NPC.position.Y <= NPC.oldPos[1].Y) && PhaseTimer > 1)
+                    {
+                        SoundEngine.PlaySound(SoundID.Item74, NPC.Center);
+                        Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, Vector2.Zero, ModContent.ProjectileType<CometEmberProj>(), 30, .05f, Main.myPlayer);
+                        NPC.active = false;
+                    }
+                    break;
+            }
+        }
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            Color color = Color.White;
+            Vector2 drawPos = NPC.Center - screenPos;
+            Texture2D origTexture = TextureAssets.Npc[NPC.type].Value;
+            Vector2 orig = origTexture.Size() / 2f;
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive, SamplerState.AnisotropicClamp, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+            for (int j = 0; j <= 10; j += 2)
+            {
+                for (int i = -60; i <= 60; i += 60)
+                {
+                    float rad = MathHelper.ToRadians(i + (i / 60f) * (Timer - j));
+                    float pulse = 1.5f + .5f * (float)Math.Sin(MathHelper.ToRadians(2 * i + (Timer - j)));
+                    Main.spriteBatch.Draw(origTexture, NPC.oldPos[j] + new Vector2(NPC.width / 2, NPC.height / 2) - screenPos, null, color * (float)((float)(12 - j) / 24), rad, orig, pulse, SpriteEffects.None, 0f);
+                }
+            }
+            for (int i = -60; i <= 60; i += 60)
+            {
+                float rad = MathHelper.ToRadians(i + (i / 60f) * Timer);
+                float pulse = 1.5f + .5f * (float)Math.Sin(MathHelper.ToRadians(2 * i + Timer));
+                Main.spriteBatch.Draw(origTexture, drawPos, null, color, rad, orig, pulse, SpriteEffects.None, 0f);
+            }
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.AnisotropicClamp, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+            return false;
+        }
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.OverworldNight.Chance * .05f;
+        }
+    }
+}
---
+++ NPCs/Enemies/CometEmberProj.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using ReLogic.Content;
+using MythosOfMoonlight.Dusts;
+
+namespace MythosOfMoonlight.NPCs.Enemies
+{
+    public class CometEmberProj : ModProjectile
+    {
+        public override string Texture => "MythosOfMoonlight/Textures/Extra/blank";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Explosion");
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 1;
+            Projectile.height = 1;
+            Projectile.aiStyle = -1;
+            Projectile.friendly = false;
+            Projectile.tileCollide = false;
+            Projectile.hostile = true;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 5;
+        }
+        public float Scale
+        {
+            get => Projectile.ai[0];
+            set => Projectile.ai[0] = value;
+        }
+        public float Alpha
+        {
+            get => Projectile.ai[1];
+            set => Projectile.ai[1] = value;
+        }
+        public override void AI()
+        {
+            Alpha = Projectile.timeLeft / 5f;
+            Scale = 10f * (2f * (5 - Projectile.timeLeft) + 1f);
+            if (Projectile.timeLeft == 4)
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    Vector2 vel = Main.rand.NextVector2Circular(7.5f, 7.5f);
+                    Dust dust = Dust.NewDustDirect(Projectile.Center, 1, 1, ModContent.DustType<PurpurineDust>());
+                    dust.velocity = vel;
+                    dust.noGravity = true;
+                    dust.scale = Main.rand.NextFloat(1f, 4f);
+                }
+            }
+        }
+        public override bool ShouldUpdatePosition()
+        {
+            return false;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D tex = ModContent.Request<Texture2D>("MythosOfMoonlight/NPCs/Enemies/RupturedPilgrim/Projectiles/PilgrimExplosion_Extra", AssetRequestMode.ImmediateLoad).Value;
+            Vector2 pos = Projectile.Center - Main.screenPosition;
+            BlendState blend = BlendState.Additive;
+            float sc = 10 * Scale / tex.Width;
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, blend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.GameViewMatrix.TransformationMatrix);
+            Main.spriteBatch.Draw(tex, pos, null, Color.White * Alpha, 0f, tex.Size() / 2f, sc, SpriteEffects.None, 0f);
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.GameViewMatrix.TransformationMatrix);
+            return false;
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            return Vector2.Distance(new Vector2(projHitbox.Center.X, projHitbox.Center.Y), new Vector2(targetHitbox.Center.X, targetHitbox.Center.Y)) <= Scale + targetHitbox.Width / 2f;
+        }
+    }
+}
