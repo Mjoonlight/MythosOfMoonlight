32d2a16335a46c3b9a5b767513d15e8aec00d284
Blink_Dagger <82193069+blinkdagger214748@users.noreply.github.com>
2022-10-03 10:18:15 +0800

    CDG-Iris!!!

---
+++ Items/IridicSet/CDGIris.cs
@@ -0,0 +1,69 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using MythosOfMoonLight.Projectiles.IridicProjectiles;
+using MythosOfMoonlight.Items.Materials;
+using Terraria.GameContent.Creative;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using MythosOfMoonlight.Projectiles.IridicProjectiles;
+using Terraria.Audio;
+
+namespace MythosOfMoonlight.Items.IridicSet
+{
+    public class CDGIris : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("CDG-Iris");
+            Tooltip.SetDefault("Cosmos Derived Gun? Cosmos Derived Fun.");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+        public override void SetDefaults()
+        {
+            Item.damage = 5;
+            Item.DamageType = DamageClass.Ranged;
+            Item.noUseGraphic = false;
+            Item.noMelee = true;
+            Item.knockBack = 1.5f;
+            Item.width = 38;
+            Item.height = 16;
+            Item.useTime = 5;
+            SoundStyle style = SoundID.Item31;
+            style.Volume = .5f;
+            Item.UseSound = style;
+            Item.useAnimation = 10;
+            Item.reuseDelay = 30;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useAmmo = AmmoID.Bullet;
+            Item.autoReuse = true;
+            Item.value = Item.buyPrice(0, 0, 0, 1);
+            Item.rare = ItemRarityID.Green;
+            Item.shoot = ModContent.ProjectileType<FragmentBullet>();
+            Item.shootSpeed = 15f;
+        }
+        public override bool RangedPrefix()
+        {
+            return true;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if (type == ProjectileID.Bullet)
+            {
+                Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<FragmentBullet>(), damage, knockback, player.whoAmI);
+            }
+            else
+            {
+                Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+            }
+            return false;
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = Recipe.Create(Type)
+                .AddIngredient(ModContent.ItemType<PurpurineQuartz>(), 15)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
---
+++ Projectiles/IridicProjectiles/FragmentBullet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using MythosOfMoonlight.Dusts;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.Audio;
+
+namespace MythosOfMoonlight.Projectiles.IridicProjectiles
+{
+    public class FragmentBullet : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fragment Bullet");
+        }
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.width = 6;
+            Projectile.height = 6;
+            Projectile.friendly = true;
+            Projectile.penetrate = 2;
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = false;
+            Projectile.timeLeft = 600;
+            Projectile.netUpdate = true;
+            Projectile.netUpdate2 = true;
+            Projectile.netImportant = true;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 15;
+        }
+        public override void AI()
+        {
+            Lighting.AddLight(Projectile.Center, new Vector3(.4f, 0, .6f));
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.Lerp(lightColor, Color.White, .4f);
+        }
+        public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Vector2 vel = -Utils.SafeNormalize(Projectile.oldVelocity, Vector2.Zero).RotatedBy(Main.rand.NextFloat(-.66f, .66f)) * Main.rand.NextFloat(1f, 2f);
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<PurpurineDust>(), vel.X, vel.Y, 0, default, Main.rand.NextFloat(.6f, 1.2f));
+                dust.noGravity = true;
+                dust.velocity = vel;
+            }
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Vector2 vel = -Utils.SafeNormalize(Projectile.oldVelocity, Vector2.Zero).RotatedBy(Main.rand.NextFloat(-.66f, .66f)) * Main.rand.NextFloat(1f, 2f);
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<PurpurineDust>(), vel.X, vel.Y, 0, default, Main.rand.NextFloat(.6f, 1.2f));
+                dust.noGravity = true;
+                dust.velocity = vel;
+            }
+            return base.OnTileCollide(oldVelocity);
+        }
+    }
+}
