27e909893ee65ceb59d93ea97640c7d7cd149b38
Blink_Dagger <82193069+blinkdagger214748@users.noreply.github.com>
2022-10-10 22:16:20 +0800

    reworked fighterGlobalAI a bit

--- NPCs/FighterGlobalAI.cs
+++ NPCs/FighterGlobalAI.cs
@@ -10,20 +10,31 @@ namespace MythosOfMoonlight.NPCs
         public override bool InstancePerEntity => true;
         const float StrideLimit = 70.78f;
         public bool Jump = false;
-        public void FighterAI(NPC NPC, float jumpHeight, float strideSpeed, bool canJump)
+        public void FighterAI(NPC NPC, float jumpHeight, float strideSpeed, bool canJump, int jumpFrame = 1, int jumpOffset = 4, float turningVel = .06f)
         {
             // var sqrDistance = player.DistanceSQ(NPC.position);
             NPC.TargetClosest(false);
-            NPC.velocity.X = MathHelper.Lerp(NPC.velocity.X, strideSpeed * NPC.direction, 0.35f);
-            var player = Main.player[NPC.target];
-            Collision.StepUp(ref NPC.position, ref NPC.velocity, NPC.width, NPC.height, ref NPC.stepSpeed, ref NPC.gfxOffY, 1, false, 0);
-            if (NPC.collideX && canJump)
+            if (strideSpeed * NPC.direction >= 0)
+            {
+                if (NPC.velocity.X < strideSpeed * NPC.direction) NPC.velocity.X += turningVel;
+            }
+            else
+            {
+                if (NPC.velocity.X > strideSpeed * NPC.direction) NPC.velocity.X -= turningVel;
+            }
+            var player = Main.player[NPC.target]; 
+            if (NPC.collideX && canJump && !Jump)
             {
                 NPC.velocity.Y = -jumpHeight;
                 Jump = true;
             }
-            if (NPC.collideY) Jump = false;
-            if (Jump) NPC.frame = new Rectangle(0, NPC.height + 4, NPC.width, NPC.height + 4);
+            if (NPC.collideY && NPC.velocity.Y >= 0) Jump = false;
+            if (Jump)
+            {
+                if (jumpFrame >= 0) NPC.frame = new Rectangle(0, NPC.height * jumpFrame + jumpOffset, NPC.width, NPC.height + jumpOffset);
+            }
+            Collision.StepUp(ref NPC.position, ref NPC.velocity, NPC.width, NPC.height, ref NPC.stepSpeed, ref NPC.gfxOffY, 1, false, 0);
+            
             var horizontalDistance = Math.Abs(NPC.Center.X - player.Center.X);
             if (horizontalDistance >= StrideLimit)
             {
