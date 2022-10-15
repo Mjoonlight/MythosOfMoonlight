b5476b7215e52f90088abd55225800625dadb500
Blink_Dagger <82193069+blinkdagger214748@users.noreply.github.com>
2022-10-15 10:12:42 +0800

    firefly staff slightly reworked

--- Projectiles/FireflyMinion.cs
+++ Projectiles/FireflyMinion.cs
@@ -17,11 +17,11 @@ namespace MythosOfMoonlight.Projectiles
         public float fireflyTimer = 0;
         public override void SetStaticDefaults()
         {
-            StaticDefaults("Firefly", 2);
+            StaticDefaults("Firefly", 2, 12, 2);
         }
         public override void SetDefaults()
         {
-            Defaults(16, 16, 4, -1, false, 2, 0);
+            Defaults(16, 16, 16, -1, false, 2, 0);
             Projectile.usesLocalNPCImmunity = true;
             Projectile.localNPCHitCooldown = 20;
             Projectile.DamageType = DamageClass.Magic;
@@ -58,9 +58,9 @@ namespace MythosOfMoonlight.Projectiles
             fireflyTimer++;
             if (fireflyTimer >= 99999) fireflyTimer = 0;
             Projectile.frame = fireflyTimer % 20 < 10 ? 0 : 1;
-            Glow(new Vector3(.6f, .6f, .2f));
+            Glow(new Vector3(.45f, .45f, .15f));
             Player owner = Main.player[Projectile.owner];
-            OwnerCheckMinions(4, (float)ModContent.ItemType<FireflyStick>());
+            OwnerCheckMinions(3, (float)ModContent.ItemType<FireflyStick>());
             MinionSort();
             if (owner.ownedProjectileCounts[Type] > 0)
             {
@@ -75,7 +75,6 @@ namespace MythosOfMoonlight.Projectiles
                     {
                         case State.rotate:
                             {
-                                Glow(new Vector3(.6f, .6f, .2f));
                                 desirePos = owner.Center;
                                 desirePos += rotateRadian.ToRotationVector2() * new Vector2(70, 15);
                                 Projectile.velocity = desirePos - Projectile.Center;
@@ -83,8 +82,11 @@ namespace MythosOfMoonlight.Projectiles
                             }
                         case State.dash:
                             {
-                                Glow(new Vector3(.9f, .9f, .3f));
                                 targetPos = Main.MouseWorld;
+                                if (Vector2.Distance(targetPos, owner.Center) > 600f)
+                                {
+                                    targetPos=Vector2.Normalize(targetPos-owner.Center)*600f+owner.Center;
+                                }
                                 dashTimer++;
                                 Vector2 dashProgress = ((MathHelper.ToRadians(dashTimer * 3).ToRotationVector2() * (Vector2.Distance(targetPos, desirePos) / 2) - new Vector2(Vector2.Distance(targetPos, desirePos) / 2, 0)) * new Vector2(1, .33f)).RotatedBy((desirePos - targetPos).ToRotation());
                                 Projectile.velocity = desirePos + dashProgress - Projectile.Center;
@@ -97,7 +99,6 @@ namespace MythosOfMoonlight.Projectiles
                             }
                         case State.back:
                             {
-                                Glow(new Vector3(.9f, .9f, .3f));
                                 desirePos = owner.Center;
                                 desirePos += rotateRadian.ToRotationVector2() * new Vector2(70, 15);
                                 dashTimer++;
@@ -132,7 +133,36 @@ namespace MythosOfMoonlight.Projectiles
             float light = Math.Max(0, -Math.Abs(60 - fireflyTimer % 120) + 20) / 15f;
             Texture2D tex = ModContent.Request<Texture2D>(Projectile.ModProjectile.Texture).Value;
             Texture2D glow = ModContent.Request<Texture2D>(Projectile.ModProjectile.Texture + "_Glow").Value;
-            SpriteEffects effect = Projectile.velocity.X >= 0 ? SpriteEffects.None : SpriteEffects.FlipVertically;
+            SpriteEffects effect = Projectile.velocity.X >= 0 ? SpriteEffects.None : SpriteEffects.FlipVertically; List<VertexInfo2> vertices = new();
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            Vector2 ori = Projectile.Size / 2;
+            Texture2D trail = ModContent.Request<Texture2D>("MythosOfMoonlight/Textures/Extra/White").Value;
+            if (state != State.rotate)
+            {
+                for (int j = 0; j <= Math.Min(16 - Projectile.timeLeft, 11); j += 1)
+                {
+                    if (Projectile.oldPos[j] != Vector2.Zero)
+                    {
+                        vertices.Add(new VertexInfo2(
+                            Projectile.oldPos[j] + ori - Main.screenPosition + (Projectile.oldRot[j] + MathHelper.PiOver2).ToRotationVector2() * 5f,
+                            new Vector3(j / (float)(Math.Min(16 - Projectile.timeLeft, 11) + 1), 0, 1 - (j / (float)(Math.Min(300 - Projectile.timeLeft, 11) + 1))),
+                            new Color(.1f, .1f, .033f) * (1 - (j / (float)(Math.Min(16 - Projectile.timeLeft, 11) + 1)))));
+                        vertices.Add(new VertexInfo2(
+                            Projectile.oldPos[j] + ori - Main.screenPosition + (Projectile.oldRot[j] - MathHelper.PiOver2).ToRotationVector2() * 5f,
+                            new Vector3(j / (float)(Math.Min(16 - Projectile.timeLeft, 11) + 1), .25f, 1 - (j / (float)(Math.Min(16 - Projectile.timeLeft, 11) + 1))),
+                            new Color(.66f, .66f, .22f) * (1 - (j / (float)(Math.Min(16 - Projectile.timeLeft, 11) + 1)))));
+                    }
+                }
+                spriteBatch.End();
+                spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive, SamplerState.AnisotropicClamp, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+                Main.graphics.GraphicsDevice.Textures[0] = trail;
+                if (vertices.Count >= 3)
+                {
+                    Main.graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleStrip, vertices.ToArray(), 0, vertices.Count - 2);
+                }
+                spriteBatch.End();
+                spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.AnisotropicClamp, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+            }
             Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, new Rectangle(0, Projectile.frame * 16, 16, 16), lightColor, Projectile.rotation, new Vector2(8), 1f, effect, 0);
             Main.EntitySpriteDraw(glow, Projectile.Center - Main.screenPosition, new Rectangle(0, Projectile.frame * 16, 16, 16), Color.White * light, Projectile.rotation, new Vector2(8), 1f, effect, 0);
             return false;
