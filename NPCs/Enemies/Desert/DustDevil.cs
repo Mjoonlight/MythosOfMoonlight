c7f098c2d149c20ed39ba222b000b813596f1f36
Ebon1 <alikhaddam42@gmail.com>
2023-03-07 21:13:30 +0100

    YOU HAVE FUCKING MOM IT CAME FREE WITH YOUR FUCKING TMODLOADER

--- NPCs/Enemies/Desert/DustDevil.cs
+++ NPCs/Enemies/Desert/DustDevil.cs
@@ -265,7 +265,7 @@ SUMMONERMELEE */
                 Dust dust2 = Main.dust[num902];
                 dust2.velocity *= 3f;
             }
-            Main.player[npc.target].velocity += b * 0.55f;
+            Main.player[npc.target].velocity += b * 0.35f;
             if (Main.player[npc.target].Center.Distance(Projectile.Center) > 750f)
                 Projectile.Kill();
 
