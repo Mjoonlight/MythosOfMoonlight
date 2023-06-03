07335b258bb48e6ba46eb7b6201efae1b82b9be9
Ebon1 <alikhaddam42@gmail.com>
2023-06-03 20:41:37 +0200

    a

--- Items/PurpleComet/PurpleCometSpawn.cs
+++ Items/PurpleComet/PurpleCometSpawn.cs
@@ -48,4 +48,48 @@ namespace MythosOfMoonlight.Items.PurpleComet
             return true;
         }
     }
+    public class AsteroidSpawn : ModItem
+    {
+        public override string Texture => "MythosOfMoonlight/Texturs/Extra/blank";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("ASTEROID's Offering");
+            Tooltip.SetDefault("Use at nighttime to call upon the ASTEROID\n" +
+                "Not ASTEROID");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 34;
+            Item.height = 18;
+            Item.rare = ItemRarityID.Pink;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.useTime = Item.useAnimation = 10;
+            Item.noMelee = true;
+            Item.autoReuse = false;
+            Item.UseSound = SoundID.Item43;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (PurpleCometEvent.PurpleComet || Main.dayTime)
+                return false;
+
+            return true;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            Main.NewText("You feel like you're ASTEROID...", 179, 0, 255);
+            SoundEngine.PlaySound(SoundID.Roar, new Vector2((int)player.position.X, (int)player.position.Y));
+            Star.starfallBoost = 3;
+            bool a = true;
+            Main.UpdateTime_StartNight(ref a);
+
+            if (Main.netMode == NetmodeID.Server)
+                NetMessage.SendData(MessageID.WorldData);
+            return true;
+        }
+    }
 }
