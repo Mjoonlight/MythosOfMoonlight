3485fc7e3d387d4b67af8c051b9457e8eb994081
Mjoonlight <mjoonlight@gmail.com>
2023-09-17 11:44:56 -0400

    over
    over

--- NPCs/Minibosses/RupturedPilgrim/RupturedPilgrim.cs
+++ NPCs/Minibosses/RupturedPilgrim/RupturedPilgrim.cs
@@ -20,6 +20,7 @@ using System.Data;
 
 namespace MythosOfMoonlight.NPCs.Minibosses.RupturedPilgrim
 {
+    [AutoloadBossHead]	
     public class RupturedPilgrim : ModNPC
     {
         public static NPC Sym => Starine_Symbol.symbol;
