fe8659a868fb22b2aed14575235731d2badefcc3
Ebon1 <alikhaddam42@gmail.com>
2024-03-04 21:10:01 +0100

    dead totems implemented, will do worldgen later

---
+++ Tiles/Rubble/DeadTotems.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MythosOfMoonlight.Tiles.Rubble
+{
+    public class DeadTotems : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+
+            TileObjectData.newTile.Height = 4;
+            TileObjectData.newTile.Width = 2;
+
+            TileObjectData.newTile.RandomStyleRange = 10;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.newTile.StyleMultiplier = 36;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16 };
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.addTile(Type);
+            Main.tileMerge[TileID.Stone][Type] = true;
+
+            DustType = DustID.Stone;
+
+            AddMapEntry(Color.Thistle);
+        }
+    }
+}
