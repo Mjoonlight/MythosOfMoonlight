9e427d786055e7aed879439fca453e4a88afc561
Ebon1 <alikhaddam42@gmail.com>
2024-07-26 00:19:21 +0200

    comet vfx change Part 1: head

--- Events/PurpleCometSky.cs
+++ Events/PurpleCometSky.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Graphics.PackedVector;
+using rail;
 using System;
 using Terraria;
 using Terraria.Graphics.Effects;
 using Terraria.ModLoader;
+using Terraria.Utilities;
 
 namespace MythosOfMoonlight.Events
 {
@@ -11,8 +14,14 @@ namespace MythosOfMoonlight.Events
     {
         public bool isActive;
         public float Intensity;
+        int innerSeed = 0, innerSeed2 = 0, outerSeed = 0;
         public override void Activate(Vector2 position, params object[] args)
         {
+            innerSeed = Main.rand.Next(int.MaxValue);
+            innerSeed2 = Main.rand.Next(int.MaxValue);
+            outerSeed = Main.rand.Next(int.MaxValue / 2);
+            linesGlowOuter = 0.3f;
+            linesGlowInner2 = -0.5f;
             isActive = true;
         }
         public override void Deactivate(params object[] args)
@@ -33,26 +42,45 @@ namespace MythosOfMoonlight.Events
             {
                 Intensity = Math.Max(0f, Intensity - 0.01f);
             }
+            linesGlowInner += 0.005f;
+            linesGlowInner2 += 0.005f;
+            linesGlowOuter += 0.0025f;
+            if (linesGlowOuter > .75f)
+            {
+                linesGlowOuter = 0f;
+                outerSeed = Main.rand.Next(int.MaxValue);
+            }
 
+            if (linesGlowInner > .75f)
+            {
+                linesGlowInner = 0f;
+                innerSeed = Main.rand.Next(int.MaxValue);
+            }
+            if (linesGlowInner2 > .75f)
+            {
+                linesGlowInner2 = 0f;
+                innerSeed2 = Main.rand.Next(int.MaxValue);
+            }
         }
         public override bool IsActive()
         {
             return Intensity > 0;
         }
-        float glow;
+        float glow, linesGlowInner, linesGlowInner2, linesGlowOuter = 0.5f;
         //float intensity;
         public override void Draw(SpriteBatch spriteBatch, float minDepth, float maxDepth)
         {
             if (maxDepth >= 3.40282347E+38f && minDepth < 3.40282347E+38f)
             {
+                DrawTrail();
                 Texture2D Tex = ModContent.Request<Texture2D>("MythosOfMoonlight/Textures/Sky").Value;
                 Texture2D starTex = ModContent.Request<Texture2D>("MythosOfMoonlight/Textures/Extra/flare").Value;
-                Texture2D starTex2 = ModContent.Request<Texture2D>("MythosOfMoonlight/Textures/Extra/star_04").Value;
+                Texture2D starTex2 = ModContent.Request<Texture2D>("MythosOfMoonlight/Textures/Extra/flameEye2").Value;
+                Texture2D starTex3 = ModContent.Request<Texture2D>("MythosOfMoonlight/Textures/Extra/Circle").Value;
                 Texture2D comet = ModContent.Request<Texture2D>("MythosOfMoonlight/Textures/Extra/comet_tail2").Value;
-                Texture2D comet2 = ModContent.Request<Texture2D>("MythosOfMoonlight/Textures/Extra/cone5").Value;
                 Vector2 Pos = new(Main.screenWidth / 2, Main.screenHeight / 2);
                 //int cometX = (int)(Main.time / 32400.0 * (double)(scen.totalWidth + (float)(comet.Width * 2))) - comet.Width;
-                Vector2 cometP = Vector2.Lerp(new Vector2(Main.screenWidth + 300, -100), new Vector2(-500, Main.screenHeight + 100), (float)Main.time / 32400);
+                Vector2 cometP = Vector2.Lerp(new Vector2(Main.screenWidth + 300, 100), new Vector2(-500, 400), (float)Main.time / 32400);
                 //new(Main.screenWidth / 4, MathHelper.Lerp(-200, Main.screenHeight + comet.Height * 0.5f, (float)Main.time / 32400));
                 if (Main.screenWidth > Tex.Width || Main.screenHeight > Tex.Height)
                     spriteBatch.Draw(Tex, new Rectangle(0, 0, Main.screenWidth, Main.screenHeight), null, Color.White * Intensity * 0.5f, 0, Vector2.Zero, SpriteEffects.None, 0);
@@ -61,25 +89,59 @@ namespace MythosOfMoonlight.Events
                 spriteBatch.Reload(BlendState.Additive);
                 glow += Main.rand.NextFloat(-.1f, .1f);
                 glow = MathHelper.Clamp(glow, 0, 1);
-                spriteBatch.Draw(comet, cometP, null, Color.White * Intensity * 0.65f * ((((float)Math.Sin((double)Main.GlobalTimeWrappedHourly) + 1) * 0.5f) + 0.4f), MathHelper.ToRadians(245), new Vector2(comet.Width / 2, 0.15f), 1, SpriteEffects.None, 0f);
-                spriteBatch.Draw(comet, cometP, null, Color.White * Intensity * 0.85f, MathHelper.ToRadians(245), new Vector2(comet.Width / 2, 0.25f), 0.95f, SpriteEffects.None, 0f);
-                //spriteBatch.Draw(comet2, cometP - Vector2.UnitY * 30, null, Color.White * Intensity * 0.75f, MathHelper.ToRadians(-90), comet2.Size() / 2, 0.5f, SpriteEffects.None, 0f);
+                //spriteBatch.Draw(comet, cometP, null, Color.White * Intensity * 0.65f * ((((float)Math.Sin((double)Main.GlobalTimeWrappedHourly) + 1) * 0.5f) + 0.4f), MathHelper.ToRadians(245), new Vector2(comet.Width / 2, 0.15f), 1, SpriteEffects.None, 0f);
+                //spriteBatch.Draw(comet, cometP, null, Color.White * Intensity * 0.85f, MathHelper.ToRadians(245), new Vector2(comet.Width / 2, 0.25f), 0.95f, SpriteEffects.None, 0f);
 
                 Vector2 starOffset = new Vector2(35, -14f);
 
-                spriteBatch.Draw(starTex, cometP + starOffset, null, Color.White * Intensity * ((((float)Math.Sin((double)Main.GlobalTimeWrappedHourly) + 1) * 0.7f) + 0.4f + glow), MathHelper.ToRadians(90), starTex.Size() / 2, 0.5f, SpriteEffects.None, 0f);
-                spriteBatch.Draw(starTex, cometP + starOffset, null, Color.White * Intensity, MathHelper.ToRadians(90), starTex.Size() / 2, 0.5f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(starTex, cometP + starOffset, null, Color.DarkViolet * Intensity * 0.1f * ((((float)Math.Sin((double)Main.GlobalTimeWrappedHourly) + 1) * 0.5f) + 0.4f + glow), Main.GameUpdateCount * 0.0035f, starTex.Size() / 2, .45f + glow * 0.1f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(starTex, cometP + starOffset, null, Color.DarkViolet * Intensity * 0.35f, Main.GameUpdateCount * -0.0035f, starTex.Size() / 2, .45f + glow * 0.1f, SpriteEffects.None, 0f);
 
-                spriteBatch.Draw(starTex, cometP + starOffset, null, Color.White * Intensity * ((((float)Math.Sin((double)Main.GlobalTimeWrappedHourly) + 1) * 0.7f) + 0.4f + glow), MathHelper.ToRadians(0), starTex.Size() / 2, 0.75f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(starTex, cometP + starOffset, null, Color.DarkViolet * Intensity, 0, starTex.Size() / 2, .15f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(starTex3, cometP + starOffset, null, Color.White * 0.65f * Intensity, 0, starTex3.Size() / 2, .5f, SpriteEffects.None, 0f);
 
-                spriteBatch.Draw(starTex, cometP + starOffset, null, Color.White * Intensity * 0.5f * ((((float)Math.Sin((double)Main.GlobalTimeWrappedHourly) + 1) * 0.5f) + 0.4f + glow), MathHelper.ToRadians(45), starTex2.Size() / 2, 0.45f, SpriteEffects.None, 0f);
-
-                spriteBatch.Draw(starTex, cometP + starOffset, null, Color.White * Intensity * 0.5f * ((((float)Math.Sin((double)Main.GlobalTimeWrappedHourly) + 1) * 0.5f) + 0.4f + glow), MathHelper.ToRadians(-45), starTex2.Size() / 2, 0.45f, SpriteEffects.None, 0f);
-
+                spriteBatch.Draw(starTex2, cometP + starOffset, null, Color.DarkViolet * Intensity * 0.1f * ((((float)Math.Sin((double)Main.GlobalTimeWrappedHourly) + 1) * 0.7f) + 0.4f + glow), Main.GameUpdateCount * 0.001f, starTex2.Size() / 2, 0.32f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(starTex2, cometP + starOffset, null, Color.DarkViolet * Intensity * 0.1f * ((((float)Math.Sin((double)Main.GlobalTimeWrappedHourly) + 1) * 0.7f) + 0.4f + glow), Main.GameUpdateCount * -0.001f, starTex2.Size() / 2, 0.32f, SpriteEffects.None, 0f);
+                DrawShine(cometP + starOffset);
             }
             spriteBatch.Reload(BlendState.AlphaBlend);
         }
+        void DrawTrail()
+        {
+            Texture2D tex = Helper.GetTex("MythosOfMoonlight/Textures/Extra/slash");
+            int trailLength = 100;
+            Vector2 starOffset = new Vector2(35, -14f);
+            for (int i = 0; i < trailLength; i++)
+            {
+                Vector2 pos = Vector2.Lerp(new Vector2(Main.screenWidth + 300, 100), new Vector2(-500, 400), (float)(Main.time - i * 50) / 32400);
+                Vector2 nextPos = Vector2.Lerp(new Vector2(Main.screenWidth + 300, 100), new Vector2(-500, 400), (float)(Main.time - (i + 1) * 50) / 32400);
 
+                Main.spriteBatch.Draw(tex, pos + starOffset, null, Color.DarkViolet * (0.7f + glow * 0.1f) * Intensity, Helper.FromAToB(pos, nextPos).ToRotation(), tex.Size() / 2, 0.1f, SpriteEffects.None, 0);
+            }
+        }
+        void DrawShine(Vector2 pos)
+        {
+            Texture2D tex = Helper.GetTex("MythosOfMoonlight/Textures/Extra/slash");
+            Main.spriteBatch.Draw(tex, pos, null, Color.DarkViolet * (0.7f + glow * 0.1f) * Intensity, 0, tex.Size() / 2, new Vector2(0.4f - glow * 0.1f, 0.2f + glow * 0.1f), SpriteEffects.None, 0);
+
+            Main.spriteBatch.Draw(tex, pos, null, Color.DarkViolet * (0.7f + glow * 0.1f) * Intensity, MathHelper.PiOver2, tex.Size() / 2, new Vector2(0.4f - glow * 0.1f, 0.2f + glow * 0.1f), SpriteEffects.None, 0);
+            for (int k = 0; k < 3; k++)
+            {
+                float linesGlow = MathHelper.Clamp(k == 0 ? linesGlowInner : k == 1 ? linesGlowOuter : linesGlowInner2, 0, 1);
+                float alpha = MathHelper.Lerp(0.75f, 0, MathHelper.Clamp(linesGlow * 1.5f, 0, 1)) * 2;
+                int seed = k == 0 ? innerSeed : k == 1 ? outerSeed : innerSeed2;
+                UnifiedRandom rand = new UnifiedRandom(seed);
+                float max = 5;
+                for (float i = 0; i < max; i++)
+                {
+                    float angle = Helper.CircleDividedEqually(i, max);
+                    float scale = rand.NextFloat(0.5f, 0.8f) + (k == 1 ? 0.25f : 0);
+                    Vector2 offset = new Vector2(Main.rand.NextFloat(30 * (k == 1 ? 1.2f : 1)) * linesGlow * scale, 0).RotatedBy(angle);
+                    for (float j = 0; j < 2; j++)
+                        Main.spriteBatch.Draw(tex, pos + offset, null, Color.DarkViolet * (k == 1 ? 0.24f : 0.4f) * alpha, angle, tex.Size() / 2, new Vector2(linesGlow, alpha) * scale, SpriteEffects.None, 0);
+                }
+            }
+        }
         public override Color OnTileColor(Color inColor)
         {
             Vector4 value = inColor.ToVector4();
