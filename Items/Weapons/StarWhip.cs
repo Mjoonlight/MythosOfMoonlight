2122de72c88778b8dc560990a69a627ab62c19d8
DarkMango <117542029+DarkMangoWasTaken@users.noreply.github.com>
2024-11-28 22:24:34 -0700

    Whip
    I'm rotting

---
+++ Items/Weapons/StarWhip.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MythosOfMoonlight.Items.Weapons
+{
+    public class StarWhip : ModItem
+    {
+        //public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(ExampleWhipDebuff.TagDamage);
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToWhip(ModContent.ProjectileType<StarWhipPro>(), 20, 2, 4);
+            Item.rare = ItemRarityID.Green;
+        }
+
+        public override bool MeleePrefix()
+        {
+            return true;
+        }
+    }
+
+    public class StarWhipPro : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.IsAWhip[Type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.DefaultToWhip();
+        }
+
+        private float Timer
+        {
+            get => Projectile.ai[0];
+            set => Projectile.ai[0] = value;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            Main.player[Projectile.owner].MinionAttackTargetNPC = target.whoAmI;
+            Projectile.damage = (int)(Projectile.damage * 0.5f);
+        }
+
+        private void DrawLine(List<Vector2> list)
+        {
+            Texture2D texture = TextureAssets.FishingLine.Value;
+            Rectangle frame = texture.Frame();
+            Vector2 origin = new Vector2(frame.Width / 2, 2);
+
+            Vector2 pos = list[0];
+            for (int i = 0; i < list.Count - 2; i++)
+            {
+                Vector2 element = list[i];
+                Vector2 diff = list[i + 1] - element;
+
+                float rotation = diff.ToRotation() - MathHelper.PiOver2;
+                Color color = Lighting.GetColor(element.ToTileCoordinates(), Color.White);
+                Vector2 scale = new Vector2(1, (diff.Length() + 2) / frame.Height);
+
+                Main.EntitySpriteDraw(texture, pos - Main.screenPosition, frame, color, rotation, origin, scale, SpriteEffects.None, 0);
+
+                pos += diff;
+            }
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            List<Vector2> list = new List<Vector2>();
+            Projectile.FillWhipControlPoints(Projectile, list);
+
+            DrawLine(list);
+
+            SpriteEffects flip = Projectile.spriteDirection < 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+
+            Vector2 pos = list[0];
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                Rectangle frame = new Rectangle(0, 0, 17, 29);
+                Vector2 origin = new Vector2(8, 0);
+                float scale = 1;
+
+                if (i == list.Count - 2)
+                {
+                    frame.Y = 78;
+                    frame.Height = 18;
+
+                    Projectile.GetWhipSettings(Projectile, out float timeToFlyOut, out int _, out float _);
+                    float t = Timer / timeToFlyOut;
+                    scale = MathHelper.Lerp(0.5f, 1.5f, Utils.GetLerpValue(0.1f, 0.7f, t, true) * Utils.GetLerpValue(0.9f, 0.7f, t, true));
+                }
+                else if (i > 10)
+                {
+                    frame.Y = 62;
+                    frame.Height = 16;
+                }
+                else if (i > 5)
+                {
+                    frame.Y = 46;
+                    frame.Height = 16;
+                }
+                else if (i > 0)
+                {
+                    frame.Y = 30;
+                    frame.Height = 16;
+                }
+
+                Vector2 element = list[i];
+                Vector2 diff = list[i + 1] - element;
+
+                float rotation = diff.ToRotation() - MathHelper.PiOver2;
+                Color color = Lighting.GetColor(element.ToTileCoordinates());
+
+                Main.EntitySpriteDraw(texture, pos - Main.screenPosition, frame, color, rotation, origin, scale, flip, 0);
+                  
+                pos += diff;
+            }
+            return false;
+        }
+    }
+}
---
+++ Items/Weapons/Starlash.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MythosOfMoonlight.Dusts;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MythosOfMoonlight.Items.Weapons
+{
+    public class Starlash : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToWhip(ModContent.ProjectileType<StarlashPro>(), 20, 2, 4);
+            Item.rare = ItemRarityID.Green;
+        }
+
+        public override bool MeleePrefix()
+        {
+            return true;
+        }
+    }
+
+    public class StarlashPro : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.IsAWhip[Type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 18;
+            Projectile.height = 18;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.ownerHitCheck = true;
+            Projectile.extraUpdates = 1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.WhipSettings.Segments = 13;
+            Projectile.WhipSettings.RangeMultiplier = 1f;
+        }
+
+        private float Timer
+        {
+            get => Projectile.ai[0];
+            set => Projectile.ai[0] = value;
+        }
+
+        public override void AI()
+        {
+            Player owner = Main.player[Projectile.owner];
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+
+            Projectile.Center = Main.GetPlayerArmPosition(Projectile) + Projectile.velocity * Timer;
+            Projectile.spriteDirection = Projectile.velocity.X >= 0f ? 1 : -1;
+
+            Timer++;
+
+            float swingTime = owner.itemAnimationMax * Projectile.MaxUpdates;
+            if (Timer >= swingTime || owner.itemAnimation <= 0)
+            {
+                Projectile.Kill();
+                return;
+            }
+
+            owner.heldProj = Projectile.whoAmI;
+            if (Timer == swingTime / 2)
+            {
+                List<Vector2> points = Projectile.WhipPointsForCollision;
+                Projectile.FillWhipControlPoints(Projectile, points);
+                SoundEngine.PlaySound(SoundID.Item153, points[points.Count - 1]);
+            }
+
+            float swingProgress = Timer / swingTime;
+
+            if (Utils.GetLerpValue(0.1f, 0.7f, swingProgress, clamped: true) * Utils.GetLerpValue(0.9f, 0.7f, swingProgress, clamped: true) > 0.5f && !Main.rand.NextBool(3))
+            {
+                List<Vector2> points = Projectile.WhipPointsForCollision;
+                points.Clear();
+                Projectile.FillWhipControlPoints(Projectile, points);
+                int pointIndex = Main.rand.Next(points.Count - 10, points.Count);
+                Rectangle spawnArea = Utils.CenteredRectangle(points[pointIndex], new Vector2(30f, 30f));
+                int dustType = ModContent.DustType<StarineDust>();
+
+                Dust dust = Dust.NewDustDirect(spawnArea.TopLeft(), spawnArea.Width, spawnArea.Height, dustType, 0f, 0f, 100, Color.White);
+                dust.position = points[pointIndex];
+                dust.fadeIn = 0.3f;
+                Vector2 spinningPoint = points[pointIndex] - points[pointIndex - 1];
+                dust.noGravity = true;
+                dust.velocity *= 0.5f;
+
+                dust.velocity += spinningPoint.RotatedBy(owner.direction * ((float)Math.PI / 2f));
+                dust.velocity *= 0.5f;
+            }
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            Item.NewItem(Projectile.GetSource_OnHit(target), target.getRect(), ModContent.ItemType<StarlashBuffPickup>());
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            List<Vector2> list = new List<Vector2>();
+            Projectile.FillWhipControlPoints(Projectile, list);
+
+            SpriteEffects flip = Projectile.spriteDirection < 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+
+            Main.instance.LoadProjectile(Type);
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+
+            Vector2 pos = list[0];
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                Rectangle frame = new Rectangle(0, 0, 18, 29);
+                Vector2 origin = new Vector2(8, 14);
+                float scale = 1;
+
+                if (i == list.Count - 2)
+                {
+                    frame.Y = 74;
+                    frame.Height = 18;
+
+                    Projectile.GetWhipSettings(Projectile, out float timeToFlyOut, out int _, out float _);
+                    float t = Timer / timeToFlyOut;
+                    scale = MathHelper.Lerp(0.5f, 1.5f, Utils.GetLerpValue(0.1f, 0.7f, t, true) * Utils.GetLerpValue(0.9f, 0.7f, t, true));
+                }
+                else if (i > 10)
+                {
+                    frame.Y = 62;
+                    frame.Height = 18;
+                }
+                else if (i > 5)
+                {
+                    frame.Y = 48;
+                    frame.Height = 18;
+                }
+                else if (i > 0)
+                {
+                    frame.Y = 30;
+                    frame.Height = 18;
+                }
+
+                Vector2 element = list[i];
+                Vector2 diff = list[i + 1] - element;
+
+                float rotation = diff.ToRotation() - MathHelper.PiOver2;
+                Color color = Color.White;
+
+                Main.EntitySpriteDraw(texture, pos - Main.screenPosition, frame, color, rotation, origin, scale, flip, 0);
+
+                pos += diff;
+            }
+            return false;
+        }
+    }
+
+    public class StarlashBuffPickup : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.ItemsThatShouldNotBeInInventory[Type] = true;
+            ItemID.Sets.IgnoresEncumberingStone[Type] = true;
+            ItemID.Sets.IsAPickup[Type] = true;
+            ItemID.Sets.ItemSpawnDecaySpeed[Type] = 4;
+            ItemID.Sets.ItemIconPulse[Item.type] = true;
+            ItemID.Sets.ItemNoGravity[Item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.height = 12;
+            Item.width = 12;
+        }
+
+        public override bool OnPickup(Player player)
+        {
+            player.AddBuff(ModContent.BuffType<StarlashBuff>(), 900);
+            SoundEngine.PlaySound(SoundID.Grab, player.Center);
+
+            return false;
+        }
+
+        public override void Update(ref float gravity, ref float maxFallSpeed)
+        {
+            Lighting.AddLight(Item.Center, Color.Cyan.ToVector3() * 0.1f);
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+    }
+
+    public class StarlashBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetDamage(DamageClass.Summon) *= 1.2f;
+        }
+    }
+}
